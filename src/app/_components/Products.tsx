f6536d4f161ce1057dff901c454bb8ea9b3ddd6f
minoru horiuchi <noru238@gmail.com>
2025-01-11 19:52:53 +0900

    トップページ：Productsサイドコンポーネント化

--- src/app/_components/Products.tsx
+++ src/app/_components/Products.tsx
@@ -1,6 +1,4 @@
-'use client'
 import { Spacer } from '@nextui-org/react'
-import { motion } from 'framer-motion'
 import { useLocale } from 'next-intl'
 import React from 'react'
 import { MoreButton } from '@/app/_components/MoreButton'
@@ -10,25 +8,19 @@ import { ToDoWithTab } from '@/app/_components/products/ToDoWithTab'
 import { getPathname } from '@/i18n/routing'
 
 export function Products() {
-  const ref = React.useRef(null)
   const locale = useLocale()
 
   return (
     <div className='px-6 py-3'>
       <h1 className='font-potta_one text-xl capitalize'>products</h1>
       <Spacer y={10} />
-      <motion.div
-        initial={{ y: 60, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, ease: 'easeOut' }}
-        ref={ref}
-      >
+      <div className='animate-fade-in w-full translate-y-5 opacity-0'>
         <div className='flex flex-col justify-between gap-10 sm:flex-row'>
           <ToDoWithTab />
           <BlogProduct />
           <SamplePage />
         </div>
-      </motion.div>
+      </div>
       <div className='mt-5 flex justify-end'>
         <MoreButton href={getPathname({ locale, href: '/products' })} />
       </div>
